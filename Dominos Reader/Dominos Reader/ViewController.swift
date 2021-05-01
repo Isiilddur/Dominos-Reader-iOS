e3723d0b693dc061d37584ba8eab95a65d16a20b
Luis Ricardo Montes <ricardo.montes@externos.alphacredit.mx>
2021-05-01 00:19:31 -0500

    Initial Commit

---
+++ Dominos Reader/Dominos Reader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dominos Reader
+//
+//  Created by Luis Montes Gómez on 01/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Dominos Reader/Dominos ReaderTests/Dominos_ReaderTests.swift
@@ -0,0 +1,33 @@
+//
+//  Dominos_ReaderTests.swift
+//  Dominos ReaderTests
+//
+//  Created by Luis Montes Gómez on 01/05/21.
+//
+
+import XCTest
+@testable import Dominos_Reader
+
+class Dominos_ReaderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
